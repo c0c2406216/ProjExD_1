7a199150f0ca56b9a46a437f50b92cc3d1a5de46
yoshidahisa <c0c2406216@edu.teu.ac.jp>
2025-06-10 16:36:11 +0900

    練習3:こうかとん画像の読み取り

--- pygame_image.py
+++ pygame_image.py
@@ -9,20 +9,36 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png")
+    bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像のSurface
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 背景画像を反転
+    kk_img = pg.image.load("fig/3.png")  # こうかとん画像のSurface
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+    kk_rct = kk_img.get_rect()  # こうかとん画像のRect
+    kk_rct.center = 300, 200  # こうかとん画像の中心座標を設定
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()  # キーの押下状態を取得
+        if key_lst[pg.K_UP]:  # 上キーが押されたら
+            kk_rct.move_ip(0, -1)  # 上に移動
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img,[300,200])
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])  # 1枚目
+        screen.blit(bg_img2, [-x+1600, 0])  # 2枚目
+        screen.blit(bg_img, [-x+3200, 0])  # 3枚目
+        # screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
+        # print(tmr, x)
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
