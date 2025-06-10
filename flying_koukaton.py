f062250ef2a5d9cc06912674fdb84ef9628109af
yoshidahisa <c0c2406216@edu.teu.ac.jp>
2025-06-10 18:36:37 +0900

    演習4.課題

--- flying_koukaton.py
+++ flying_koukaton.py
@@ -14,31 +14,42 @@ def main():
     kk_img = pg.image.load("fig/3.png")  # こうかとん画像のSurface
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_rct = kk_img.get_rect()  # こうかとん画像のRect
-    kk_rct.center = 300, 200  # こうかとん画像の中心座標を設定
-    x = 0
+    kk_rct.center = 300, 200 
+     # こうかとん画像の中心座標を設定
+    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
-        x = (x + 1) % 3200
-        kk_rct.move_ip(-1, 0)
+        x =tmr % 3200
+        
+        if key_lst:
+            RL = -1
+            UD = 0
+
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            RL = -1
+            UD = -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
+            RL = -1
+            UD = 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            RL = -1
+            UD = 0
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
-        x %= 3200
+            RL = 1
+            UD = 0
+        
+        kk_rct.move_ip(RL, UD)
+        x =tmr% 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         # screen.blit(kk_img, [300, 200])
+        
         screen.blit(kk_img,kk_rct)
         pg.display.update()
-        # tmr += 1        
-        # clock.tick(10)
+        tmr += 1        
         clock.tick(200)
 
 
